3342ad2a719c7a5a4685d5fc04c2bff98b632a24
Hossein <hossein.bachari2000@gmail.com>
2026-06-12 08:49:54 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#incluse<iostream>
+using namespace std;
+int recursive_sum(int m , in n)
+if (m == n)
+return m + recursive_sum (m+1 , n)
+
+int  main(){
+  int x =2 , y=4;
+cout<<reursive_sum(int m , int n);
